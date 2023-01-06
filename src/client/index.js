0405dca7e09927de03871baa7a3be516c9803981
FatimahAlIbrahim <fatimah.al.ibrahim.1997@gmail.com>
2023-01-06 16:21:58 +0300

    finished primary design and added image handling to webpack

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,11 @@
 import './styles/resets.scss'
 import './styles/style.scss'
+import pinIcon from './images/pushpin.png'
+
+let pinImages = document.getElementsByClassName("card-pin");
+
+for(let i = 0; i < pinImages.length; i++){
+    pinImages[i].src = pinIcon;
+}
 
 alert("I Exist!")
